ad8e198023cdc0dc9bc16dd6a3617d105d483fda
sonal-alt <74061846+sonal-alt@users.noreply.github.com>
2021-03-05 04:32:20 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+
+function sq(){
+
+ result=document.getElementById("num").value;
+
+ Number=(Math.sqrt(result))
+
+document.getElementById("paragraph").innerHTML=(Number);
+
+};
